3cb635f9ca2e1dc2a22b8c2b24efa5e56053daa7
Kris Arnold <karnold@dma.org>
2014-08-01 15:43:06 -0500

    Added badge model and controllers

--- models/Badge.php
+++ models/Badge.php
@@ -1,9 +1,39 @@
-<?php
-
-namespace DMA\Friends\Models
+<?php namespace DMA\Friends\Models;
 
 use Model;
 
-class Badge extends Model {
-    protected $table = 'dma_badges';
+/**
+ * Badge Model
+ */
+class Badge extends Model
+{
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'dma_friends_badges';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $hasOne = [];
+    public $hasMany = [];
+    public $belongsTo = [];
+    public $belongsToMany = [];
+    public $morphTo = [];
+    public $morphOne = [];
+    public $morphMany = [];
+    public $attachOne = [];
+    public $attachMany = [];
+
 }
