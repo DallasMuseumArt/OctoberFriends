d60f82ae8b694a3a62f01c91bf90d307dceeec2e
Kristen Arnold <karnold@dma.org>
2014-08-14 12:31:06 -0500

    Added a basic reporting widge to display number of friends by day and week

---
+++ reportwidgets/FriendsToolbar.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DMA\Friends\ReportWidgets;
+
+use Backend\Classes\ReportWidgetBase;
+use Rainlab\User\Models\User;
+
+class FriendsToolbar extends ReportWidgetBase
+{
+    public $defaultAlias = 'friendsToolbar';
+
+    public function widgetDetails()
+    {
+        return [
+            'name'        => 'Friends Toolbar',
+            'description' => 'Show some basic statistics on friends'
+        ];
+    }
+
+    public function render()
+    {
+        $today = date('Y-m-d');
+        $thisWeek = date('Y-m-d', strtotime('last monday'));
+
+        $this->addCss('css/friendstoolbar.css');
+
+        $this->vars['numFriends'] = number_format(User::count());
+        $this->vars['todayFriends'] = number_format(User::where('created_at', '>=', $today)->count());
+        $this->vars['weekFriends'] = number_format(User::where('created_at', '>=', $thisWeek)->count());
+
+        return $this->makePartial('widget');
+    }
+}
