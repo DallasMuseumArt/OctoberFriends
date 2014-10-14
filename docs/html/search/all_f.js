bd3fdd6d29efcb88092cd74f20f97c3e7ddeccf6
Kris Arnold <karnold@dma.org>
2014-10-14 15:23:53 -0500

    Added time restriction widget

--- docs/html/search/all_f.js
+++ docs/html/search/all_f.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['time_5frestrict_5fdays',['TIME_RESTRICT_DAYS',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#a71b85478f20cda144aeffe010364a0f7',1,'DMA::Friends::Models::Activity']]],
   ['time_5frestrict_5fhours',['TIME_RESTRICT_HOURS',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#ac78040e8784e02c2d1bcce5221ac6cb8',1,'DMA::Friends::Models::Activity']]],
-  ['time_5frestrict_5fnone',['TIME_RESTRICT_NONE',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#ab9dd8b18c4810beabdcf8e45039913c8',1,'DMA::Friends::Models::Activity']]]
+  ['time_5frestrict_5fnone',['TIME_RESTRICT_NONE',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#ab9dd8b18c4810beabdcf8e45039913c8',1,'DMA::Friends::Models::Activity']]],
+  ['timerestrictions',['TimeRestrictions',['../dc/d09/classDMA_1_1Friends_1_1FormWidgets_1_1TimeRestrictions.html',1,'DMA::Friends::FormWidgets']]]
 ];
--- docs/html/search/classes_a.js
+++ docs/html/search/classes_a.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['viewmaker',['ViewMaker',['../d7/d1b/classDMA_1_1Friends_1_1Components_1_1ViewMaker.html',1,'DMA::Friends::Components']]]
+  ['user',['User',['../dd/d73/classDMA_1_1Friends_1_1Wordpress_1_1User.html',1,'DMA::Friends::Wordpress']]],
+  ['userbadges',['UserBadges',['../d8/d01/classDMA_1_1Friends_1_1Components_1_1UserBadges.html',1,'DMA::Friends::Components']]],
+  ['usermeta',['Usermeta',['../d6/d87/classDMA_1_1Friends_1_1Models_1_1Usermeta.html',1,'DMA::Friends::Models']]]
 ];
