99a4cef242f5666bba78b46a280a12e6c0cc3290
Carlos Arroyo <carlos.arroyo@dma.org>
2014-10-17 13:09:02 -0500

    - Fixed Admin interface for group creation and manipulation

--- controllers/Groups.php
+++ controllers/Groups.php
@@ -9,14 +9,14 @@ use Backend\Classes\Controller;
 class Groups extends Controller
 {
     public $implement = [
+    	'Backend.Behaviors.ListController',    
         'Backend.Behaviors.FormController',
-        'Backend.Behaviors.ListController',
         'Backend.Behaviors.RelationController',
     ];
-
+   
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
-    public $relationConfig = 'config_relation.yaml';
+    public $relationConfig = 'config_relations.yaml';
 
     public function __construct()
     {
@@ -24,4 +24,27 @@ class Groups extends Controller
 
         BackendMenu::setContext('DMA.Friends', 'friends', 'groups');
     }
+
+    protected function execPageAction($actionName, $parameters)
+    {
+    	\Log::info($actionName);
+    	\Log::info($parameters);
+    	return parent::execPageAction($actionName, $parameters);
+    }
+    
+    public function update($recordId, $context = null){
+    	//throw new \Exception('' . get_object_vars($this));
+    	\Log::info(get_class($this));
+
+    	
+    	return $this->asExtension('FormController')->update($recordId, $context);
+    }
+    
+	public function update_onSave($recordId, $context = null)
+	{
+	
+	    // Call the FormController behavior update() method
+	    return $this->asExtension('FormController')->update_onSave($recordId, $context);
+	}   
+    
 }
