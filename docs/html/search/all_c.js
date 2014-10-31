11f4637ba5e666c6d04f2fd1ccae16ead8511487
Kristen Arnold <karnold@dma.org>
2014-10-31 14:53:38 -0500

    updates to documentation

--- docs/html/search/all_c.js
+++ docs/html/search/all_c.js
@@ -8,5 +8,5 @@ var searchData=
   ['post',['Post',['../d2/de2/classDMA_1_1Friends_1_1Wordpress_1_1Post.html',1,'DMA::Friends::Wordpress']]],
   ['prepareformfields',['prepareFormFields',['../dd/d00/classDMA_1_1Friends_1_1FormWidgets_1_1ActivityType.html#ab8b5e6c785f85176a4884d3218b1b064',1,'DMA::Friends::FormWidgets::ActivityType']]],
   ['preparevars',['prepareVars',['../dd/d00/classDMA_1_1Friends_1_1FormWidgets_1_1ActivityType.html#abb8585f86e795e2f9bfd6bea78a2f7ab',1,'DMA::Friends::FormWidgets::ActivityType']]],
-  ['process',['process',['../d2/d5d/classDMA_1_1Friends_1_1Activities_1_1ActivityCode.html#a3de9ef142396ba94532566598b508d58',1,'DMA\Friends\Activities\ActivityCode\process()'],['../d3/d35/classDMA_1_1Friends_1_1Classes_1_1ActivityTypeBase.html#a772667f95a25c6aa44e31ce9c6903384',1,'DMA\Friends\Classes\ActivityTypeBase\process()']]]
+  ['process',['process',['../d2/d5d/classDMA_1_1Friends_1_1Activities_1_1ActivityCode.html#a3de9ef142396ba94532566598b508d58',1,'DMA\Friends\Activities\ActivityCode\process()'],['../d2/d05/classDMA_1_1Friends_1_1Activities_1_1LikeWorkOfArt.html#a814a0bc1a22cc72f91dac635c871b43c',1,'DMA\Friends\Activities\LikeWorkOfArt\process()'],['../d3/d35/classDMA_1_1Friends_1_1Classes_1_1ActivityTypeBase.html#a772667f95a25c6aa44e31ce9c6903384',1,'DMA\Friends\Classes\ActivityTypeBase\process()']]]
 ];
