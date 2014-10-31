e21b396c2d15818966fe758bd67e8dcb18a5353e
Kris Arnold <karnold@dma.org>
2014-10-31 14:53:38 -0500

    updates to documentation

--- docs/html/search/classes_8.js
+++ docs/html/search/classes_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['reward',['Reward',['../d7/dc4/classDMA_1_1Friends_1_1Models_1_1Reward.html',1,'DMA::Friends::Models']]],
   ['reward',['Reward',['../d6/d94/classDMA_1_1Friends_1_1Wordpress_1_1Reward.html',1,'DMA::Friends::Wordpress']]],
+  ['reward',['Reward',['../d7/dc4/classDMA_1_1Friends_1_1Models_1_1Reward.html',1,'DMA::Friends::Models']]],
   ['rewardresource',['RewardResource',['../d2/d30/classDMA_1_1Friends_1_1Api_1_1RewardResource.html',1,'DMA::Friends::Api']]],
   ['rewards',['Rewards',['../d2/dab/classDMA_1_1Friends_1_1Controllers_1_1Rewards.html',1,'DMA::Friends::Controllers']]],
   ['rewardtransformer',['RewardTransformer',['../da/d2c/classDMA_1_1Friends_1_1Api_1_1RewardTransformer.html',1,'DMA::Friends::Api']]]
--- docs/html/search/functions_c.js
+++ docs/html/search/functions_c.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['savedata',['saveData',['../d0/d48/classDMA_1_1Friends_1_1Classes_1_1ActivityManager.html#aab92d7f0071021fdfef61d81ccc4224e',1,'DMA\Friends\Classes\ActivityManager\saveData()'],['../d3/d35/classDMA_1_1Friends_1_1Classes_1_1ActivityTypeBase.html#a69d28814a4cc0c3798d1ab7a95e14ef3',1,'DMA\Friends\Classes\ActivityTypeBase\saveData()']]],
+  ['scopefindactivitytype',['scopeFindActivityType',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#aa17825817b73d3073e37d5e2c0933003',1,'DMA::Friends::Models::Activity']]],
   ['scopefindcode',['scopefindCode',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#a9bb36aab36ff22a29b2a594617dd591c',1,'DMA::Friends::Models::Activity']]],
+  ['scopefindwordpress',['scopefindWordpress',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#a5a533a3f638f99d1dd0a8e0550def49e',1,'DMA::Friends::Models::Activity']]],
   ['scopeisactive',['scopeIsActive',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#acd3f12824d95be07b5221cdc9d64abf7',1,'DMA::Friends::Models::Activity']]],
   ['settimerestrictiondataattribute',['setTimeRestrictionDataAttribute',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#ade0ffca24448c217def365d104e52866',1,'DMA::Friends::Models::Activity']]],
   ['show',['show',['../d9/dda/classDMA_1_1Friends_1_1Api_1_1BaseResource.html#a5ac5d4fb30ec5434fd685e044c357f97',1,'DMA::Friends::Api::BaseResource']]],
