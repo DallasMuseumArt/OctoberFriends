6e7acabdc3f3f35e9140f7458ff6d8f29dee1962
Kristen Arnold <karnold@dma.org>
2014-11-05 13:24:57 -0600

    updated documentation

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['cancomplete',['canComplete',['../d3/d35/classDMA_1_1Friends_1_1Classes_1_1ActivityTypeBase.html#af5ddc6ea4b9d888873e6e499beaea396',1,'DMA::Friends::Classes::ActivityTypeBase']]],
+  ['cancomplete',['canComplete',['../d3/d35/classDMA_1_1Friends_1_1Classes_1_1ActivityTypeBase.html#acc0d37915d9170364f381945fd01f7c2',1,'DMA::Friends::Classes::ActivityTypeBase']]],
   ['categories',['Categories',['../d9/daa/classDMA_1_1Friends_1_1Controllers_1_1Categories.html',1,'DMA::Friends::Controllers']]],
   ['category',['Category',['../de/d78/classDMA_1_1Friends_1_1Models_1_1Category.html',1,'DMA::Friends::Models']]],
   ['categoryresource',['CategoryResource',['../d6/d3b/classDMA_1_1Friends_1_1Api_1_1CategoryResource.html',1,'DMA::Friends::Api']]],
   ['categorytransformer',['CategoryTransformer',['../d1/d3f/classDMA_1_1Friends_1_1Api_1_1CategoryTransformer.html',1,'DMA::Friends::Api']]],
   ['checkin',['checkin',['../d4/d33/classDMA_1_1Friends_1_1Classes_1_1FriendsLog.html#ae6c33847a3d3f094a3bd27ac2dbbfa72',1,'DMA::Friends::Classes::FriendsLog']]],
+  ['converttime',['convertTime',['../d3/d35/classDMA_1_1Friends_1_1Classes_1_1ActivityTypeBase.html#adb196a8d338da688f10f4a8da6480fd0',1,'DMA::Friends::Classes::ActivityTypeBase']]],
   ['converttimerestricteddata',['convertTimeRestrictedData',['../d2/de2/classDMA_1_1Friends_1_1Wordpress_1_1Post.html#a427b14dd99893217929ee8ee93977628',1,'DMA::Friends::Wordpress::Post']]],
   ['convertvalues',['convertValues',['../d2/de2/classDMA_1_1Friends_1_1Wordpress_1_1Post.html#a8969b8476b3ba0afc871532f6f936514',1,'DMA::Friends::Wordpress::Post']]],
   ['create',['create',['../d9/dda/classDMA_1_1Friends_1_1Api_1_1BaseResource.html#a2e62ea3d73fab798a5319189b1d134e0',1,'DMA::Friends::Api::BaseResource']]],
--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -10,5 +10,6 @@ var searchData=
   ['getsavedata',['getSaveData',['../dd/d00/classDMA_1_1Friends_1_1FormWidgets_1_1ActivityType.html#a2cfdaad84b2f972cb3df04badf77b251',1,'DMA\Friends\FormWidgets\ActivityType\getSaveData()'],['../dc/d09/classDMA_1_1Friends_1_1FormWidgets_1_1TimeRestrictions.html#a7b7880124cd7d2ec31b97baf75992860',1,'DMA\Friends\FormWidgets\TimeRestrictions\getSaveData()']]],
   ['getthemepartialsdir',['getThemePartialsDir',['../df/da2/classDMA_1_1Friends_1_1Components_1_1Modal.html#ac809c15fb0a9fc3b810afb9cc1d31ee8',1,'DMA::Friends::Components::Modal']]],
   ['gettimerestrictiondataattribute',['getTimeRestrictionDataAttribute',['../dc/d8c/classDMA_1_1Friends_1_1Models_1_1Activity.html#a98a904c739aca30f2a26152a481164b6',1,'DMA::Friends::Models::Activity']]],
+  ['gettimezoneoptions',['getTimezoneOptions',['../d1/d8a/classDMA_1_1Friends_1_1Models_1_1Settings.html#a347eae65e8aeb114767d5e4fc8542fea',1,'DMA::Friends::Models::Settings']]],
   ['getviewpath',['getViewPath',['../d7/d10/classDMA_1_1Friends_1_1Classes_1_1ActivityForm.html#a67f1c67ad0885f8ade7cc055db01d36a',1,'DMA::Friends::Classes::ActivityForm']]]
 ];
