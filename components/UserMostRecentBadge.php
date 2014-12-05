72cfad4487cdb1bd8745157a84ba735b87a6c747
Kris Arnold <karnold@dma.org>
2014-12-05 13:14:04 -0600

    Introduc a most recent badge component and support badge timestamps in "Time Ago" format. Along with other general badge bug fixes

---
+++ components/UserMostRecentBadge.php
@@ -0,0 +1,35 @@
+<?php namespace DMA\Friends\Components;
+
+use Cms\Classes\ComponentBase;
+use Auth;
+use View;
+use Lang;
+
+class UserMostRecentBadge extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Most Recent User Badge',
+            'description' => 'Shows the most recent badge a user has earned'
+        ];
+    }
+
+    public function onRun()
+    {
+        $user = Auth::getUser();
+
+        if (!$user) return;
+
+        $badge = $user->badges()->first();
+
+        if (!$badge) {
+            $this->page['badge'] = Lang::get('dma.friends::lang.badges.noBadges');
+            return;
+        }
+
+        $this->page['badge'] = View::make('dma.friends::badgePreview', ['badge' => $badge])->render();
+        
+    }
+}
