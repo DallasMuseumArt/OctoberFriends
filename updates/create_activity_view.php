61048fd3503b4aa2b98ddbf6529cd248bd6c949f
Kris Arnold <karnold@dma.org>
2014-12-09 12:59:53 -0600

    Updates to make activity stream functional

--- updates/create_activity_view.php
+++ updates/create_activity_view.php
@@ -14,35 +14,48 @@ class CreateActivityView extends Migration {
     {   
         DB::statement('CREATE VIEW dma_friends_activity_stream AS
 
-            SELECT 
-                activity_id as object_id, 
-                created_at, 
-                user_id, 
-                "DMA\\Friends\\Models\\Activity" as object_type
-            FROM dma_friends_activity_user
-            UNION
-            SELECT 
-                badge_id as object_id, 
-                created_at, 
-                user_id, 
-                "DMA\\Friends\\Models\\Badge" as object_type
-            FROM dma_friends_badge_user
-            UNION
-            SELECT 
-                reward_id as id, 
-                created_at, 
-                user_id, 
-                "DMA\\Friends\\Models\\Reward" as object_type
-            FROM dma_friends_reward_user
-            UNION
-            SELECT 
-                step_id as object_id, 
-                created_at, 
-                user_id, 
-                "DMA\\Friends\\Models\\Step" as object_type
-            FROM dma_friends_step_user
-
-            ORDER BY created_at DESC
+            CREATE 
+                ALGORITHM = UNDEFINED 
+                DEFINER = `root`@`localhost` 
+                SQL SECURITY DEFINER
+            VIEW `dma_friends_activity_stream` AS
+                select 
+                    `a`.`title` AS `title`,
+                    `pivot`.`activity_id` AS `object_id`,
+                    `pivot`.`created_at` AS `created_at`,
+                    `pivot`.`user_id` AS `user_id`,
+                    'Activity' COLLATE utf8_unicode_ci AS `object_type` 
+                from
+                    (`dma_friends_activity_user` `pivot`
+                    left join `dma_friends_activities` `a` ON ((`pivot`.`activity_id` = `a`.`id`))) 
+                union select 
+                    `a`.`title` AS `title`,
+                    `pivot`.`badge_id` AS `object_id`,
+                    `pivot`.`created_at` AS `created_at`,
+                    `pivot`.`user_id` AS `user_id`,
+                    'Badge' COLLATE utf8_unicode_ci AS `object_type`
+                from
+                    (`dma_friends_badge_user` `pivot`
+                    left join `dma_friends_badges` `a` ON ((`pivot`.`badge_id` = `a`.`id`))) 
+                union select 
+                    `a`.`title` AS `title`,
+                    `pivot`.`reward_id` AS `id`,
+                    `pivot`.`created_at` AS `created_at`,
+                    `pivot`.`user_id` AS `user_id`,
+                    'Reward' COLLATE utf8_unicode_ci AS `object_type`
+                from
+                    (`dma_friends_reward_user` `pivot`
+                    left join `dma_friends_rewards` `a` ON ((`pivot`.`reward_id` = `a`.`id`))) 
+                union select 
+                    `a`.`title` AS `title`,
+                    `pivot`.`step_id` AS `object_id`,
+                    `pivot`.`created_at` AS `created_at`,
+                    `pivot`.`user_id` AS `user_id`,
+                    'Step' COLLATE utf8_unicode_ci AS `object_type`
+                from
+                    (`dma_friends_step_user` `pivot`
+                    left join `dma_friends_steps` `a` ON ((`pivot`.`step_id` = `a`.`id`)))
+                order by `created_at` desc
         ');
     }   
 
