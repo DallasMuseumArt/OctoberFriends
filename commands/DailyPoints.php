34b56b246d55e0d807fd2a28f7d21cfc87cd3435
Kris Arnold <karnold@dma.org>
2014-12-10 12:05:56 -0600

    Added crontasks to clear daily and weekly points for users
    Enhanced leaderboard widget to display this information

---
+++ commands/DailyPoints.php
@@ -0,0 +1,43 @@
+<?php
+namespace DMA\Friends\Commands;
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Indatus\Dispatcher\Scheduling\Schedulable;
+use Indatus\Dispatcher\Scheduling\ScheduledCommand;
+use DB;
+
+/**
+ * Set a cron task that will reset the points all users have earned for the week back to zero
+ *
+ * @package DMA\Friends\Commands
+ * @author Kristen Arnold, Carlos Arroyo
+ */
+class DailyPoints extends ScheduledCommand
+{
+    /**
+     * @var string The console command name.
+     */
+    protected $name = 'friends:points-daily';
+
+    /**
+     * When a command should run
+     * @param Scheduler $scheduler
+     * @return \Indatus\Dispatcher\Scheduling\Schedulable
+     */
+    public function schedule(Schedulable $scheduler)
+    {
+        // Run every 5 minutes
+        return $scheduler->daily();
+    }
+
+    /**
+     * Set the number of points set per week back to zero on sunday morning
+     * @return void
+     */
+    public function fire()
+    {
+        DB::table('users')
+            ->update(['points_today' => 0]);
+    }
+}
