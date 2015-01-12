8f91c9b17600160a5bb130a7059aa1e4aaff66fe
Kristen Arnold <karnold@dma.org>
2015-01-12 15:42:13 -0600

    Fix time restriction settings for activities

---
+++ updates/fix_time_restrictions.php
@@ -0,0 +1,32 @@
+<?php namespace DMA\Friends\Updates;
+
+use October\Rain\Database\Updates\Migration;
+use DMA\Friends\Models\Activity;
+
+class FixTimeRestrictions extends Migration {
+
+    /** 
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        Activity::chunk(200, function($activities) {
+            foreach ($activities as $activity) {
+                if ($activity->date_begin && $activity->date_end) {
+                    $activity->time_restriction = Activity::TIME_RESTRICT_DAYS;
+                } elseif (!empty($activity->time_restriction_data)) {
+                    $activity->time_restriction = Activity::TIME_RESTRICT_HOURS;
+                } else {
+                    $activity->time_restriction = Activity::TIME_RESTRICT_NONE;
+                }
+                
+                $activity->save();
+
+            }
+        });
+    }     
+
+}
+
