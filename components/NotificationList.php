726b1027cdbb7778d61155d0c4025e804adf6294
Carlos Arroyo <carlos.arroyo@dma.org>
2015-01-15 23:50:23 +1100

    - Improved UI to allow user to mark as read notifications. - Added option to limit the number of notifications to display. - Added notification counter to partial header. - Grouped notifications by day when rendering component. - Notification model clean up and minor tweaks.

--- components/NotificationList.php
+++ components/NotificationList.php
@@ -4,6 +4,7 @@ use Request;
 use Auth;
 use Redirect; 
 use Cms\Classes\ComponentBase;
+use DMA\Friends\Models\Notification;
 
 use System\Classes\ApplicationException;
 
@@ -22,10 +23,22 @@ class NotificationList extends ComponentBase
     {
         return [
             'onlyUnread' => [
-                'title'     => 'Display only unread notifications',
-                'type'      => 'checkbox',
-                'default'   => true
-            ]   
+                    'title'             => 'Show only unread',
+                    'description'       => 'Display only unread notifications',
+                    'type'              => 'checkbox',
+            ],
+            'autoMarkAsRead' => [
+                    'title'             => 'Auto mark as read',
+                    'description'       => 'Mark as read after display for first time',
+                    'type'              => 'checkbox',
+            ],
+            'showLimit' => [
+                    'title'             => 'Show maximum',
+                    'description'       => 'Show maximum a given number of notifications',
+                    'validationPattern' => '^\d+$',
+                    'validationMessage' => 'Value should be a positive integer',
+                    'default'           => 20
+            ]
         ];
     }
     
@@ -38,14 +51,26 @@ class NotificationList extends ComponentBase
     protected function getNotifications()
     {
         if($user = $this->getUser()){
+            // Get query to notifications and expire old notifications
             $query = $user->notifications();
+            $query = $query->expire(); // Expire old notifications
             
             if ($this->property('onlyUnread')){
-                $query = $query->unread();   
+                $query = $query->unread();
             }
             
+            // Limit 
+            $limit = $this->property('showLimit');
+            $unReadCount = $user->notifications()
+                                ->unread()
+                                ->count();
+            
+            $limit = ( $limit < $unReadCount ) ? $unReadCount : $limit;
+            
             // sort by creation
-            $query = $query->orderBy('created_at','desc');
+            $query = $query->orderBy('created_at','desc')
+                           ->take($limit);
+            
             
             return $query->get()->toArray();
         }else{
@@ -77,16 +102,28 @@ class NotificationList extends ComponentBase
     public function onRun()
     {
         // Inject CSS and JS
-        //$this->addCss('components/grouprequest/assets/css/group.request.css');
-        //$this->addJs('components/grouprequest/assets/js/group.request.js');
+        $this->addCss('components/notificationlist/assets/css/notification.list.css');
+        $this->addJs('components/notificationlist/assets/js/notification.list.js');
         
         // Populate page user and other variables
     	$this->prepareVars();
-    	//$this->markAllAsRead();
+    	
+    	if($this->property('autoMarkAsRead')){
+    	   $this->markAllAsRead();
+        }
     
     }    
    
     
+    ## AJAX
+    public function onMarkRead()
+    {
+        if (($id = post('id')) != ''){
+            if($notification = Notification::find($id)){
+                $notification->markAsRead();
+            }
+        }
+    }
 
     
   
