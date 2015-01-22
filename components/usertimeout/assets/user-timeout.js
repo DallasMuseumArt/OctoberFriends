319caf240cfa0ee49d419b80f7ac2823d5a240b1
Kristen Arnold <karnold@dma.org>
2015-01-22 09:21:53 -0600

    fixed bug in timeout interval

--- components/usertimeout/assets/user-timeout.js
+++ components/usertimeout/assets/user-timeout.js
@@ -1,21 +1,23 @@
 (function($) {
 
     var idleTime = 0;
-    var timer = modalTimeout;
+    var timer = modalTimeout * 1000;
 
     $(document).ready(function() {
         //Automatically logout authorized kiosks after a specific amount of inactivity
         $('a.usertimeoutHandler').hide();
 
         //Increment the idle time counter every minute.
-        var idleInterval = setInterval(idleTimeout, timer * 1000); // 10 seconds
+        var idleInterval = setInterval(idleTimeout, timer);
 
         //Zero the idle timer on mouse movement.
         $(this).mousemove(function (e) {
-            idleTime = 0;
+            clearInterval(idleInterval);
+            idleInterval = setInterval(idleTimeout, timer);
         });
         $(this).keypress(function (e) {
-            idleTime = 0;
+            clearInterval(idleInterval);
+            idleInterval = setInterval(idleTimeout, timer);
         });
 
         function idleTimeout() {
