14b3c3ade24c3b7bdc3a21e8ace99046c9aed08e
Kristen Arnold <karnold@dma.org>
2015-02-05 14:51:52 -0600

    updated documentation

--- docs/html/search/classes_9.js
+++ docs/html/search/classes_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['normalizeuserdata',['NormalizeUserData',['../dc/d26/classDMA_1_1Friends_1_1Commands_1_1NormalizeUserData.html',1,'DMA::Friends::Commands']]],
   ['notification',['Notification',['../de/d7f/classDMA_1_1Friends_1_1Models_1_1Notification.html',1,'DMA::Friends::Models']]],
   ['notificationcounter',['NotificationCounter',['../d7/def/classDMA_1_1Friends_1_1Components_1_1NotificationCounter.html',1,'DMA::Friends::Components']]],
   ['notificationlist',['NotificationList',['../dc/d03/classDMA_1_1Friends_1_1Components_1_1NotificationList.html',1,'DMA::Friends::Components']]],
