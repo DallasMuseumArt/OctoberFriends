b5f966f749a28c7c3ed9eba0ef7c48eca8a55be3
Kristen Arnold <karnold@dma.org>
2015-02-17 13:09:42 -0600

    order categories alphabetically

--- models/Badge.php
+++ models/Badge.php
@@ -55,7 +55,11 @@ class Badge extends Model
     ];
 
     public $morphToMany = [
-        'categories'    => ['DMA\Friends\Models\Category', 'name' => 'object', 'table' => 'dma_friends_object_categories'],
+        'categories'    => ['DMA\Friends\Models\Category', 
+            'name'  => 'object', 
+            'table' => 'dma_friends_object_categories',
+            'order' => 'name',
+        ],
     ];
 
     public function scopeNotCompleted($query, User $user)
