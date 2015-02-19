28afd36a8fd2d504d560f7bd6c53e7f26928e593
Kristen Arnold <karnold@dma.org>
2015-02-19 13:49:29 -0600

    Added new friends by day report widget

--- reportwidgets/FriendsToolbar.php
+++ reportwidgets/FriendsToolbar.php
@@ -4,6 +4,7 @@ namespace DMA\Friends\ReportWidgets;
 
 use Backend\Classes\ReportWidgetBase;
 use Rainlab\User\Models\User;
+use DB;
 
 class FriendsToolbar extends ReportWidgetBase
 {
@@ -30,10 +31,34 @@ class FriendsToolbar extends ReportWidgetBase
 
         $this->addCss('css/friendstoolbar.css');
 
-        $this->vars['numFriends'] = number_format(User::count());
-        $this->vars['todayFriends'] = number_format(User::where('created_at', '>=', $today)->count());
-        $this->vars['weekFriends'] = number_format(User::where('created_at', '>=', $thisWeek)->count());
+        $this->vars['numFriends']       = number_format(User::count());
+        $this->vars['todayFriends']     = number_format(User::where('created_at', '>=', $today)->count());
+        $this->vars['weekFriends']      = number_format(User::where('created_at', '>=', $thisWeek)->count());
+        $this->vars['averageFriends']   = number_format($this->getAverageFriends());
 
         return $this->makePartial('widget');
+    }
+
+    public function getAverageFriends()
+    {
+        $average = DB::select(
+            DB::raw("
+                SELECT 
+                    AVG(numFriends) as avgNum
+                FROM
+                    (
+                        SELECT 
+                            DAYOFWEEK(created_at) AS dow,
+                            DATE(created_at) AS d,
+                            COUNT(*) AS numFriends
+                        FROM users
+                        GROUP BY d
+                    ) AS countFriends
+                WHERE dow = DAYOFWEEK(NOW())
+                GROUP BY dow
+            ")
+        );
+
+        return $average[0]->avgNum;
     }
 }
