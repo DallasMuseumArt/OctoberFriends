486e3d003db1b3e5816ff66190f383565dbb00ee
Kristen Arnold <karnold@dma.org>
2015-05-15 21:22:09 -0500

    allow user to change graph type

--- reportwidgets/graphreport/assets/graphreport.js
+++ reportwidgets/graphreport/assets/graphreport.js
@@ -1,50 +1,68 @@
-var friendsReports = {};
-friendsReports.graphs = {};
-
 (function($) {
 
-    /**
-     * Provides some default settings to use across all graphs
-     */
-    friendsReports.defaultGraphSettings = {
-        color: {
-            pattern: ['#95B753', '#CC3300'],
-        },
-        zoom: {
-            enabled: true,
-            rescale: true,
-        },
-    }
+    friendsReports = {
+        graphs: {},
 
-    friendsReports.init = function() {
-        for (var key in friendsReports.graphs) {
-            this.processRequest(friendsReports.graphs[key]);
-        }
-    }
-
-    friendsReports.initGraph = function(obj, settings) {
-        $.extend(settings, friendsReports.defaultGraphSettings);
-
-        obj.renderedGraph = c3.generate(settings);
-    }
-
-    friendsReports.processRequest = function(obj) {
-        $.ajax({
-            url: obj.ajaxPath,
-            dataType: 'json',
-            data: {
-                to: friendsReports.toDate,
-                from: friendsReports.fromDate,
+        /**
+         * Provides some default settings to use across all graphs
+         */
+        defaultGraphSettings: {
+            color: {
+                pattern: ['#95B753', '#CC3300'],
             },
-            type: 'GET',
-            error: function() {
-                alert('something went wrong');
+            zoom: {
+                enabled: true,
+                rescale: true,
             },
-            success: function(data) {
-                obj.data = data;
-                obj.chart();
+        },
+
+        init: function() {
+            for (var key in friendsReports.graphs) {
+                this.processRequest(friendsReports.graphs[key]);
             }
-        });
-    }
+
+            this.handleControls();
+        },
+
+        initGraph: function(obj, settings) {
+            $.extend(settings, friendsReports.defaultGraphSettings);
+
+            obj.renderedGraph = c3.generate(settings);
+        },
+
+        processRequest: function(obj) {
+            $.ajax({
+                url: obj.ajaxPath,
+                dataType: 'json',
+                data: {
+                    to: friendsReports.toDate,
+                    from: friendsReports.fromDate,
+                },
+                type: 'GET',
+                error: function() {
+                    alert('something went wrong');
+                },
+                success: function(data) {
+                    obj.data = data;
+                    obj.chart();
+                }
+            });
+        },
+
+        handleControls: function() {
+            var $this = this;
+
+            $('.graph .controls a').on('click', function(e) {
+                e.preventDefault();
+
+                var type = $(this).data('type');
+                var parentId = $(this).parents('.graph').data('graphid');
+
+console.log($this);
+                $this.graphs[parentId].renderedGraph.transform(type);
+                console.log(parentId);
+            });
+        }
+    };
 
 })(window.jQuery);
