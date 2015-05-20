3b67856785530a5f163ea8adc4e704ac441dfd4a
Carlos Arroyo <carlos.arroyo@dma.org>
2015-05-20 10:08:34 +1000

    Added redeem endpoint to rewards API

--- api/resources/RewardResource.php
+++ api/resources/RewardResource.php
@@ -1,6 +1,13 @@
 <?php namespace DMA\Friends\API\Resources;
 
+use Session;
+use Response;
+use DMA\Friends\Models\Reward;
+use DMA\Friends\Classes\RewardManager;
 use DMA\Friends\Classes\API\BaseResource;
+
+use RainLab\User\Models\User;
+
 
 class RewardResource extends BaseResource {
 
@@ -8,4 +15,55 @@ class RewardResource extends BaseResource {
 
     protected $transformer  = '\DMA\Friends\API\Transformers\RewardTransformer';
 
+    public function __construct()
+    {
+        // Add additional routes to Activity resource
+        $this->addAdditionalRoute('redeem',   'redeem/{reward}/user/{user}',   ['GET', 'POST']);
+    }
+    
+    public function redeem($rewardId, $userId)
+    {
+        try{
+            
+            if(is_null($user = User::find($userId))){
+                return Response::api()->errorNotFound('User not found');
+            }
+            
+            if(is_null($reward = Reward::find($rewardId))){
+                return Response::api()->errorNotFound('Reward not found');
+            }
+            
+            RewardManager::redeem($rewardId, $user);
+            
+            // Check if redeem was successful 
+            $message = Session::pull('rewardMessage');
+            $type    = ($message) ? 'info' : 'error'; 
+            
+            $success = true;
+            $httpCode = 200;
+            
+            
+            if($type == 'error'){
+                $success = false;
+                $httpCode = 201;
+                $message = Session::pull('rewardError');
+            }
+            
+    
+            $payload = [
+                    'data' => [
+                            'success' => $success,
+                            'message' => $message
+                    ]
+            ];
+            
+            return Response::api()->setStatusCode($httpCode)->withArray($payload);
+            
+             
+        } catch(Exception $e) {
+            return Response::api()->errorInternalError($e->getMessage());
+        }
+    }
+    
+    
 }
