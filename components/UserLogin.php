0b6fe2296189111e1d5cc95eb101f25ca69b6e66
Kristen Arnold <karnold@sfmoma.org>
2016-02-28 18:23:26 -0600

    allow all files in avatar directory to be used

--- components/UserLogin.php
+++ components/UserLogin.php
@@ -184,8 +184,9 @@ class UserLogin extends ComponentBase
         $avatars = [];
 
         $themePath = UserLogin::getThemeDir();
-        $avatarPath = $themePath . '/assets/images/avatars/*.jpg';
+        $avatarPath = $themePath . '/assets/images/avatars/*';
 
+\Debugbar::info($avatarPath);
         // loop through all the files in the plugin's avatars directory and parse the file names
         foreach ( glob($avatarPath ) as $file ) { 
             $path = str_replace(base_path(), '', $file);
